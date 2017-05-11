6ddb800a9f29aad6c080a4892250958691f4115c
aschnur <alexander.schnur@greenwichschools.org>
2017-05-11 12:26:07 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -138,3 +138,51 @@ if (someday > today) {
 document.getElementById("demo").innerHTML = text;
 
 
+
+function addWorkout() {
+	//search reading
+	//if user has no database create database using writeUserData
+	distance = document.getElementById("distance").value;
+
+	var hours = document.getElementById("hours").value;
+	hours = parseInt(hours*360);
+	var minutes = document.getElementById("minutes").value;
+	minutes = parseInt(minutes*60);
+	var seconds = document.getElementById("seconds").value;
+	seconds =parseFloat(seconds);
+	//seconds = parseInt(seconds);
+	time = hours + minutes + seconds;
+	
+	var split = document.getElementById("splitLength").value;
+	numOfSplit = distance/500;
+	numOfSplit = time/numOfSplit;
+	var i = parseInt(numOfSplit/60);
+	var t = numOfSplit%60;
+	t = Math.round(t * 10) / 10;
+
+	if (t < 10) {
+		t = "0" + t;
+	}
+
+	numOfSplit = i + ":" + t;
+
+
+	
+	//writeNewPost(date, distance, time, split);
+	updateTable();
+}
+	
+function updateTable() {
+	var table = document.getElementById("wOuts");
+	var row = table.insertRow(rowNum);
+	rowNum++;
+	var cellDate = row.insertCell(0);
+	var cellDistance = row.insertCell(1);
+	var cellTime = row.insertCell(2);
+	var cellSplit = row.insertCell(3);
+
+	cellDate.innerHTML = today;
+	cellDistance.innerHTML = distance;
+	cellTime.innerHTML = time;
+	cellSplit.innerHTML = numOfSplit;
+}
